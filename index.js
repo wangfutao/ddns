c5f52ff69d970f3974ba4062b282d0c3de4515f6
wangft <582752141@qq.com>
2023-01-31 20:46:05 +0800

    新增保存更改后的ipv6地址到文件的功能

--- index.js
+++ index.js
@@ -3,29 +3,51 @@ const path = require("path")
 const os = require('os');
 const axios = require("axios");
 
+//读取的配置文件config.json
 let config = null;
+
+//上次更新的ipv6地址
+let preIpv6 = '';
+
+//读取配置文件config.json
 fs.readFile(path.join(__dirname, 'config.json'), 'utf8', (err, data) => {
     config = JSON.parse(data);
-    startInterval();
+
+    //读取上次更新的ipv6地址
+    fs.readFile(path.join(__dirname, 'ipv.6'), 'utf8', (err, data) => {
+        data = data.trim();
+        preIpv6 = data;
+
+        //开始
+        startInterval();
+    })
 })
 
+
+//查询ip地址的次数
 let queryCount = 1;
+
+//更新域名解析的次数
 let updateCount = 1;
 
+//开始循环
 function startInterval() {
 
     setDDNS();
+
     let interval = parseInt(config.interval);
     let timer = setInterval(() => {
         setDDNS();
     }, interval);
+
 }
 
-let preIpv6 = '';
 
 function setDDNS() {
+    //获取ipv6地址
     let ipv6 = getIpv6();
     if (!ipv6) return;
+
     log('第', queryCount++, '次查询ipv6');
     if (ipv6 == preIpv6) {
         log('当前ipv6地址没有发生改变');
@@ -34,6 +56,7 @@ function setDDNS() {
     log('发现新的ipv6地址:', ipv6);
     log('第', updateCount++, '次修改ipv6');
 
+    //循环遍历配置文件里的所有域名解析配置
     let ddnsConfigs = config.ddns;
     for (let ddnsConfig of ddnsConfigs) {
         let name = ddnsConfig.name;
@@ -46,6 +69,7 @@ function setDDNS() {
 
         let method = ddnsConfig.method;
 
+        //调用接口更新域名解析
         axios.request({
             method: method,
             url: api,
@@ -54,6 +78,7 @@ function setDDNS() {
             let respData = resp.data;
             if (resp.status == 200) {
                 preIpv6 = ipv6;
+                fs.writeFile(path.join(__dirname, 'ipv.6'), ipv6, (err)=>{});
                 log('更新', name, '的ipv6地址', ipv6, '状态：success')
             } else {
                 log('更新', name, '的ipv6地址', ipv6, '状态：filed')
@@ -66,6 +91,7 @@ function setDDNS() {
     }
 }
 
+//获取本机ipv6地址
 function getIpv6() {
     const interfaces = os.networkInterfaces();
     for (const devName in interfaces) {
@@ -81,7 +107,7 @@ function getIpv6() {
     return null;
 }
 
-
+//打印日志
 function log(...data) {
     console.log(new Date(), ...data);
 }
